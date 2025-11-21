add92dec404e0e3599d90e75a0fcd6a062909127
Omid <omid.rezayof@utexas.edu>
2025-11-21 14:55:37 -0600

    add rotate_x rotate_y rotate_z methods for rotation class

--- se3kit/rotation.py
+++ se3kit/rotation.py
@@ -99,6 +99,33 @@ class Rotation:
         return is_identity(self.m)
 
     @staticmethod
+    def rotate_x(theta, degrees=False):
+        theta = deg2rad(theta) if degrees else theta
+        return Rotation(
+            np.array(
+                [[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]]
+            )
+        )
+
+    @staticmethod
+    def rotate_y(theta, degrees=False):
+        theta = deg2rad(theta) if degrees else theta
+        return Rotation(
+            np.array(
+                [[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [-np.sin(theta), 0, np.cos(theta)]]
+            )
+        )
+
+    @staticmethod
+    def rotate_z(theta, degrees=False):
+        theta = deg2rad(theta) if degrees else theta
+        return Rotation(
+            np.array(
+                [[np.cos(theta), -np.sin(theta), 0], [np.sin(theta), np.cos(theta), 0], [0, 0, 1]]
+            )
+        )
+
+    @staticmethod
     def from_zyx(euler, extrinsic=False, degrees=False):
         """
         Creates a Rotation from ZYX (default intrinsic) Euler angles.
